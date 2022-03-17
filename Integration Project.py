6859fa3b5896c7a2c785d70feb06a36a5eafd925
kaallaire1877 <101837783+kaallaire1877@users.noreply.github.com>
2022-03-17 17:30:11 -0400

    Add files via upload

---
+++ Integration Project.py
@@ -0,0 +1,90 @@
+#Kelly Allaire 
+
+## Here is my program to make the perfect Iced Coffee
+## The general equation for iced coffee is
+#### IcedCoffee = Ice + Coffee + Cream + Flavor + Sugar
+
+print("Hello, Good Morning!")
+
+#Say hello to greet your user
+
+print("How did you sleep last night?")
+input()
+
+#You need to put the input after your question so that it is
+#interactive and your user can respond.
+
+print("What is on your agenda for today?")
+input()
+
+#Now it is time to get into making the perfect coffee
+
+print("Have you had a cup of coffe yet?")
+input()
+
+print("Would you like me to help you make the perfect coffee?")
+input()
+
+print("Do you prefer hot or iced coffee?")
+input()
+
+# Keep asking questions so that they feel involved.
+## It also is more of a converstaion which people like
+## instead of it just being a computer program.
+
+print("If you like iced coffee add 2 cups of ice into a 24oz tumbler")
+print("or if you are feeling fancy add 1 1/2 cups to a glass mason jar.")
+print("Now for the important part.")
+
+# Add a little humor
+
+print("Do you need to brew your coffee or do you have cold brew?")
+input()
+
+print("If you need to brew your coffee, pop a k-cup into your keurig.")
+print("While we wait for that to brew, let's do some trivia.")
+
+### Maybe later down the road, give them an option to skip this part
+### but for right now it says.
+
+# Personally I love trivia and it is a great time killer while
+# they need to brew their coffee. What trivia is better than
+# math trivia?
+
+print("What is 3 * 5?")
+input()
+print("The answer is ... 15!")
+
+# REMEBER that 4^2 is 16 not 8!!!! 
+
+print("What is 4 ** 2?")
+input()
+print("The answer is ... 16!")
+
+
+print("What is 9 / 3")
+input()
+print("The answer is .... 3")
+
+print("What is 3612 // 2")
+input()
+print("The answer is ...." , 3612 // 2)
+
+
+print("What is 3 + 2 - 4?" , end = " ")
+
+## This end will put the answer right at the end of the line and not on a new line.
+## This isn't needed but it is kind of different. 
+
+input()
+print("The answer is ... 1!")
+
+## This was mainly a way to get all the requirements in
+#Now time to wrap it up
+
+print("WAY TO GO!!!!" , " Your coffee should be ready." , sep= " &" )
+print("Today is a good day to have a great day.")
+
+## I know that we learned in class today about how to have the question changed on
+# a specfic answer, but I am still learning that. So I didn't want to risk it,
+# so I just did stuff I was confident in. 
